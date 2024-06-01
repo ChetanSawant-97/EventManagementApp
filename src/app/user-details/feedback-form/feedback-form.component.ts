b7bba42f8ef578cb2c99430601c596e418963a3c
Chetan Sawant <csawant@microproindia.com>
2024-06-01 19:44:34 +0530

    Feedback Form

--- src/app/user-details/feedback-form/feedback-form.component.ts
+++ src/app/user-details/feedback-form/feedback-form.component.ts
@@ -1,12 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
+import { FormGroup, ReactiveFormsModule, FormBuilder, Validators} from '@angular/forms';
 @Component({
   selector: 'app-feedback-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './feedback-form.component.html',
   styleUrl: './feedback-form.component.scss'
 })
-export class FeedbackFormComponent {
 
+export class FeedbackFormComponent {
+  submitFeedback:any;
+
+  constructor(private formBuilder:FormBuilder){}
+
+  ngOnInit():void{
+    this.submitFeedback = this.formBuilder.group({
+      userName:["", Validators.required],
+      sessionAttended:["", Validators.required],
+      description:["",Validators.required]
+    })
+  }
+
+  onSubmitFeedback(){
+    if(this.submitFeedback.valid){
+      alert(JSON.stringify(this.submitFeedback.value));
+    }else{
+      alert("Invalid Field Values");
+    }
+  }
 }

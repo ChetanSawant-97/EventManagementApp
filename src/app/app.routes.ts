f7e2d7c84f74297b2d82dd0f625bf2187d49599a
Chetan Sawant <csawant@microproindia.com>
2024-06-03 11:29:51 +0530

    Lazy Loading Implemented

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,31 +1,38 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './home/login/login.component';
-import { SignupComponent } from './home/signup/signup.component';
 import { UserDetailsComponent } from './user-details/user-details.component';
 import { DashboardComponent } from './user-details/dashboard/dashboard.component';
-import { CreateEventComponent } from './user-details/create-event/create-event.component';
-import { FeedbackFormComponent } from './user-details/feedback-form/feedback-form.component';
-import { BookTicketComponent } from './user-details/book-ticket/book-ticket.component';
-import { HistoryComponent } from './user-details/history/history.component';
-import { RegisterEventComponent } from './user-details/register-event/register-event.component';
 
 export const routes: Routes = [
     {path:"",component:HomeComponent,
         children:[
-            {path:"login", component:LoginComponent},
-            {path:"signup", component:SignupComponent}
+            {path:"login",
+                loadComponent:()=>import('./home/login/login.component').then(m=>m.LoginComponent)
+            },
+            {path:"signup", 
+                loadComponent:()=>import('./home/signup/signup.component').then(m=>m.SignupComponent)
+            }
         ]
      },
      {
         path:"user",component:UserDetailsComponent,
         children:[
             {path:"", component:DashboardComponent},
-            {path:"createEvent", component:CreateEventComponent},
-            {path:"registerEvent", component:RegisterEventComponent},
-            {path:"feedback", component:FeedbackFormComponent},
-            {path:"bookTicket", component:BookTicketComponent},
-            {path:"history", component:HistoryComponent}
+            {path:"createEvent",
+                loadComponent:()=>import('./user-details/create-event/create-event.component').then(m=>m.CreateEventComponent)
+            },
+            {path:"registerEvent", 
+                loadComponent:()=>import('./user-details/register-event/register-event.component').then(m=>m.RegisterEventComponent),
+            },
+            {path:"feedback",
+                loadComponent:()=>import('./user-details/feedback-form/feedback-form.component').then(m=>m.FeedbackFormComponent)
+            },
+            {path:"bookTicket", 
+                loadComponent:()=>import('./user-details/book-ticket/book-ticket.component').then(m=>m.BookTicketComponent)
+            },
+            {path:"history", 
+                loadComponent:()=>import('./user-details/history/history.component').then(m=>m.HistoryComponent)
+            }
         ]
      }
 ];
